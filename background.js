6f441d97ed00b2820c9b7f722896393e52fb3c00
deadorbit <138953036+industrialsociety@users.noreply.github.com>
2024-05-21 10:32:29 -0500

    Create background.js

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'download') {
+    chrome.downloads.download({
+      url: message.url,
+      filename: message.filename
+    }, (downloadId) => {
+      sendResponse({downloadId: downloadId});
+    });
+    return true; // Will respond asynchronously.
+  }
+});
